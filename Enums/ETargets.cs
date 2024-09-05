8f3904db22abcb302cb14804c9a78a0c4c1e2430
Eytan zichel <eytanzichel@gmail.com>
2024-09-05 12:46:31 +0300

    Targets enum

---
+++ Enums/ETargets.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestWeek_1.Enums
+{
+    internal enum ETargets
+    {
+        WEBSERVER,
+        DATABASE,
+        USERCREDENTIALS,
+        OTHER
+    }
+}
