4a682a31607204a246fe6971a715f11f93bce84f
Eytan zichel <eytanzichel@gmail.com>
2024-09-05 12:48:13 +0300

    Defens object

---
+++ Defence.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestWeek_1.TSB
+{
+    internal class Defence
+    {
+        public int MinSeverity { get; set; }
+        public int MaxSeverity { get; set; }
+        public List<string> Defenses { get; set; }
+        public Defence Left { get; set; }
+        public Defence Right { get; set; }
+    }
+}
