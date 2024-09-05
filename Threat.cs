de29e0074b1ae3e1dad46fa8248c6f18905de2e0
Eytan zichel <eytanzichel@gmail.com>
2024-09-05 12:46:52 +0300

    Target object

---
+++ Threat.cs
@@ -0,0 +1,12 @@
+﻿using TestWeek_1.Enums;
+
+namespace TestWeek_1
+{
+    internal class Threat
+    {
+        public string ThreatType {  get; set; }
+        public int Volume {  get; set; }
+        public int Sophistication {  get; set; }
+        public ETargets Targets { get; set; }
+    }
+}
