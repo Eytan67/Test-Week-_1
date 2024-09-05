ab0bdc9bb252d7dbb8cfc25dcb91fdebb4d93467
Eytan zichel <eytanzichel@gmail.com>
2024-09-05 13:28:21 +0300

    Generic Function to load json file

--- Program.cs
+++ Program.cs
@@ -1,8 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TestWeek_1.TSB;
+using System.Text.Json;
+using TestWeek_1.Loaders;
 
 namespace TestWeek_1
 {
@@ -11,6 +15,19 @@ namespace TestWeek_1
 
         static void Main(string[] args)
         {
+            BinarySearchTree tree = new BinarySearchTree();
+            string path = "C:\\Users\\user\\source\\repos\\data\\Tests\\Week_1\\TestWeek_1\\defenceStrategiesBalanced.json";
+
+            var defences = Loder<Defence>.Load(path);
+
+            foreach (Defence defence in defences)
+            {
+                tree.Insert(defence);
+            }
+            
+            
+            
+            Console.ReadLine();
         }
     }
 }
---
+++ utils/Loder.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Text.Json;
+using System.Threading.Tasks;
+using TestWeek_1.Interfaces;
+using TestWeek_1.TSB;
+
+namespace TestWeek_1.Loaders
+{
+    internal class Loder<T>
+    {
+        public static List<T> Load(string path)
+        {
+            string jsonString = File.ReadAllText(path);
+            var items = JsonSerializer.Deserialize<List<T>>(jsonString);
+
+            return items;
+        }
+
+    }
+}
